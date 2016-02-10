a62538db181a2e96beb295b0850f6250f18eaa03
Bartosz Krol <adrilan.evenwood@gmail.com>
2016-02-10 18:49:25 +0100

    Use token env variable

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var Botkit = require('botkit');
 var controller = Botkit.slackbot();
+
 var bot = controller.spawn({
-  token: 'xoxb-20867201298-PEWuqGWzLgyviK3KsTcEiEPO'
-})
-bot.startRTM(function(err,bot,payload) {
+  token: process.env.BOT_TOKEN
+});
+
+bot.startRTM(function (err, bot, payload) {
   if (err) {
     throw new Error('Could not connect to Slack');
   }
