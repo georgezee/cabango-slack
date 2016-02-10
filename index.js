801ded576fa030b1d8921fb243456c6ee31b524c
Bartosz Krol <adrilan.evenwood@gmail.com>
2016-02-10 19:03:35 +0100

    Add simple slash command

--- index.js
+++ index.js
@@ -16,3 +16,16 @@ controller.hears('hello', 'direct_message,direct_mention,mention', function (bot
 
   bot.reply(message, 'Hello yourself.');
 });
+
+controller.setupWebserver(process.env.PORT || 5000, function(err, express_webserver) {
+  controller.createWebhookEndpoints(express_webserver)
+});
+
+controller.on('vote', function(bot, message) {
+
+  // reply to slash command
+  bot.replyPublic(message,'Everyone can see this part of the slash command');
+  bot.replyPrivate(message,'Only the person who used the slash command can see this.');
+
+});
+
